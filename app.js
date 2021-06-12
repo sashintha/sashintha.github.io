15986537803e1d84cfd59083023dd927557b7100
Sashintha Tennakoon <stennak@uwo.ca>
2021-06-12 18:51:52 -0400

    adjusted layout for sections

--- app.js
+++ app.js
@@ -14,15 +14,10 @@ const navSlide = ()=>{
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.5}s`;
             }
         });
-
         //burger ani
         burger.classList.toggle('toggle');
 
-
     });
-
-    
 }
-
 
 navSlide();
